a55b283ade115b51354b1b3fca90938685c864a3
AlexisFAP <alexispatinoagudelo@gmail.com>
2022-05-02 23:27:49 -0500

    tracks, all routes and controllers ready

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { getItems, getItem, createItem, deleteItem, updateItem } = require("../controllers/tracks");
-const { validatorCreateItem } = require('../validators/tracks');
+const { validatorCreateItem, validatorGetItem } = require('../validators/tracks');
 const customHeader = require('../middleware/customHeader');
 
 router.get("/", getItems);
 
-router.get("/:id", getItem);
+router.get("/:id", validatorGetItem, getItem);
 
-router.post('/', validatorCreateItem, customHeader, createItem);
+router.post('/', validatorCreateItem, createItem);
+
+router.put('/:id', validatorGetItem, validatorCreateItem, updateItem)
+
+router.delete('/:id', validatorGetItem, deleteItem)
+
+
 
 module.exports = router
--- validators/tracks.js
+++ validators/tracks.js
@@ -16,4 +16,12 @@ const validatorCreateItem = [
     (req, res, next) => {return validateResults(req, res, next)}
 ];
 
-module.exports = { validatorCreateItem };
+const validatorGetItem = [
+    check("id")
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+    (req, res, next) => {return validateResults(req, res, next)}
+];
+
+module.exports = { validatorCreateItem, validatorGetItem };
